2963c7dc4aedcdefe87f4ea13f524df46e5fd023
carlosanguix <carlos.anguix@gmail.com>
2020-02-12 11:40:54 +0100

    modificando rutas

--- src/server.js
+++ src/server.js
@@ -7,9 +7,8 @@ const app = express();
 const port = process.env.port || 3003;
 
 // Rutas
-// TODO No hago nada en estas rutas
-require('./routes/routes.js');
-
+// TODO Funcionamiento??????
+require('./app/routes/beers.routes')(app);
 
 // Servicio al cliente /public
 // TODO static??? Yo quiero dynamic
@@ -20,9 +19,6 @@ app.listen(port, () => {
     console.log(' * Servidor escuchando en el puerto ' + port);
 });
 
-
 //*********************** Pruebas!!
 // TODO Conectamos la BBDD (aqui para que?)
-const db = require('./database.js');
-let a = new db();
-a.connect();
+
