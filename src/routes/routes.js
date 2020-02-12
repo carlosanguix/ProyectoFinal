17c77d983fec8003303e83ab39d56bcf3a5167b5
carlosanguix <carlos.anguix@gmail.com>
2020-02-12 11:42:50 +0100

    cambiando nombre rutas

--- src/routes/routes.js
+++
@@ -1,6 +0,0 @@
-// Aquí indicaremos todas las rutas a las instrucciones según las peticiones del cliente.
-
-// Exportamos el módulo para que lo pueda recibir app.use(require('./routes/routes.js'))
-module.exports = (routes) => {
-
-}
