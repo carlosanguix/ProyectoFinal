c198843759662fd26063104732761f0833e87c9e
Carlos Anguix Genovés <carlos.anguix@gmail.com>
2020-02-17 19:55:22 +0100

    clase

--- src/app/model/comments.model.js
+++ src/app/model/comments.model.js
@@ -1,8 +1,10 @@
-class Beer {
+class Comment {
 
+    /*
     constructor(idBeer, name, origin) {
         this.idBeer = idBeer;
         this.name   = name;
         this.origin = origin;
     }
+    */
 }
